0cb90064a438a39a8135138c4fe9a43595dd1a84
John Wiegley <johnw@newartisans.com>
2018-01-23 13:13:17 -0800

    Disable a powerline9k configuration option

--- config/home.nix
+++ config/home.nix
@@ -102,7 +102,7 @@ let home_directory = builtins.getEnv "HOME";
 
     sessionVariables = {
       POWERLEVEL9K_PROMPT_ON_NEWLINE = "true";
-      POWERLEVEL9K_RPROMPT_ON_NEWLINE = "true";
+      # POWERLEVEL9K_RPROMPT_ON_NEWLINE = "true";
     };
 
     shellAliases = {
