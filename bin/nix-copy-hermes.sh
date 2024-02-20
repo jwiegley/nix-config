f45d5bd25539e01bdae5a7d628a65afd77e8cbd1
John Wiegley <johnw@newartisans.com>
2024-02-20 09:39:07 -0800

    changes

--- bin/nix-copy-hermes.sh
+++ bin/nix-copy-hermes.sh
@@ -2,7 +2,7 @@
 
 # u sign
 
-nix copy --to "ssh-ng://hermes" /nix/var/nix/profiles/per-user/johnw/profile
+nix copy --to "ssh-ng://hermes" ~/.local/state/nix/profiles/profile
 
 for project in $(grep "^[^#]" ~/.config/projects)
 do
