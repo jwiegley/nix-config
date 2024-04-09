c72971f58258040c61a8414a82210fa7bff194f2
John Wiegley <johnw@newartisans.com>
2024-04-09 09:16:17 -0700

    changes

--- overlays/00-last-known-good.nix
+++ overlays/00-last-known-good.nix
@@ -5,21 +5,12 @@ let nixpkgs = args@{ rev, sha256 }:
         owner = "NixOS";
         repo  = "nixpkgs"; })) {};
 in {
-  # gitAndTools = pkgs.gitAndTools // {
-  #   inherit ((nixpkgs {
-  #     # rev    = "known-good-20221221_180856";
-  #     rev    = "f590aeafc4deddb5bb770a85bae208f36d2c72b3";
-  #     sha256 = "0jr03wdwa2snxs9i6m1ndlh0gsz9m2crvfz5ar1xkracr9bma0n2";
-  #   }).gitAndTools) git-annex;
-  # };
-
   inherit (nixpkgs {
     # rev    = "known-good-20221221_180856";
     rev    = "f590aeafc4deddb5bb770a85bae208f36d2c72b3";
     sha256 = "0jr03wdwa2snxs9i6m1ndlh0gsz9m2crvfz5ar1xkracr9bma0n2";
   })
   httm
-  # recoll
   ;
 
   inherit (nixpkgs {
@@ -27,7 +18,6 @@ in {
     rev    = "6ac50c5df158c3cbe29c5111962e4c89daf1f929";
     sha256 = "sha256-trkGHaFfydle3MzKOFhGI4g1pY+j3s8xVsTNmztbLCI=";
   })
-  # pandoc
   mitmproxy
   ;
 
@@ -40,14 +30,6 @@ in {
   texinfo413
   ;
 
-  # inherit (nixpkgs {
-  #   # rev    = "known-good-20231214_230317";
-  #   rev    = "bd0b831a74765264d4f71035ad1d28728ba49edb";
-  #   sha256 = "sha256-V4T55d2FxUTWH2sEWpp3du+cQZ/OibR/OYw2pN5y0b0=";
-  # })
-  # backblaze-b2
-  # ;
-
   inherit (nixpkgs {
     # rev    = "known-good-20240108_120429";
     rev    = "6498fa6d61651dbe0a101992e9dd34d939ce034a";
@@ -55,8 +37,5 @@ in {
   })
   csvkit
   svg2tikz
-  # dovecot_fts_xapian
-  # libvirt
-  # xdg-utils
   ;
 }
