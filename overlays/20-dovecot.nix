2d1ea97f7810ef076de9047513470d18b6bce632
John Wiegley <johnw@newartisans.com>
2024-06-18 18:27:17 -0700

    Restore =dovecot_fts_xapian= definition

--- overlays/20-dovecot.nix
+++ overlays/20-dovecot.nix
@@ -2,14 +2,12 @@ self: super: {
 
 dovecot_fts_xapian = super.dovecot_fts_xapian.overrideAttrs(attrs: rec {
   meta = attrs.meta // { broken = false; };
-  version = "1.7.13";
-  patches = [ ./dovecot/pagesize.patch ];
+  version = "1.7.10";
   src = super.fetchFromGitHub {
     owner = "grosjo";
     repo = "fts-xapian";
     rev = version;
-    sha256 = "03m27jgm9v03f5cri9dn3zjlllb45ygdp9szikqd2c0yr83msasr";
-    # date = "2024-05-30T12:28:17Z";
+    sha256 = "sha256-Yd14kla33qAx4Hy0ZdE08javvki3t+hCEc3OTO6YfkQ=";
   };
 });
 
