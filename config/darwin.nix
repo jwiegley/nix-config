082e5d58333426d2a73598366e3ca861a1e1ee4b
John Wiegley <johnw@newartisans.com>
2025-10-09 16:15:59 -0700

    darwin: Comment out Chainweb and IOG binary caches
    - **darwin.nix**: Disable nixcache.chainweb.com and cache.iog.io substituters
      and their associated trusted public keys by commenting them out in nix
      settings, leaving only cache.nixos.org and newartisans.com active

--- config/darwin.nix
+++ config/darwin.nix
@@ -292,18 +292,18 @@ in {
       # Custom binary caches for better package availability
       substituters = [
         "https://cache.nixos.org"
-        "https://nixcache.chainweb.com"
-        "https://cache.iog.io"
+        # "https://nixcache.chainweb.com"
+        # "https://cache.iog.io"
       ];
       trusted-substituters = [
-        "https://nixcache.chainweb.com"
-        "https://cache.iog.io"
+        # "https://nixcache.chainweb.com"
+        # "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "newartisans.com:RmQd/aZOinbJR/G5t+3CIhIxT5NBjlCRvTiSbny8fYw="
-        "nixcache.chainweb.com:FVN503ABX9F8x8K0ptnc99XEz5SaA4Sks6kNcZn2pBY="
-        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+        # "nixcache.chainweb.com:FVN503ABX9F8x8K0ptnc99XEz5SaA4Sks6kNcZn2pBY="
+        # "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
     };
 
