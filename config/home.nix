4f84412a31afa13ca88aab75f930d75316b77f54
John Wiegley <johnw@newartisans.com>
2025-11-24 11:57:19 -0800

    home: Change external_host to data.newartisans.com
    - **home.nix**: Update external_host variable from "home.newartisans.com" to
      "data.newartisans.com" to reflect new primary domain for external services

--- config/home.nix
+++ config/home.nix
@@ -8,7 +8,7 @@ let home           = config.home.homeDirectory;
     master_key     = "4710CF98AF9B327BB80F60E146C4BD1A7AC14BA2";
     signing_key    = "12D70076AB504679";
 
-    external_host  = "home.newartisans.com";
+    external_host  = "data.newartisans.com";
 
     ca-bundle_path = "${pkgs.cacert}/etc/ssl/certs/";
     ca-bundle_crt  = "${ca-bundle_path}/ca-bundle.crt";
